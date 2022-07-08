ef139186d51535b4b885abcbb9ecfc1f4c22d28e
Dariya <dkladova@list.ru>
2022-07-08 12:14:53 +0300

    add example005_Condish

---
+++ example005_Condish/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ввведите имя пользователя: ");
+string? username = Console.ReadLine();
+
+if (username == "Маша")
+{
+   Console.WriteLine("Ура,это же МАША");
+}
+else 
+{
+   Console.Write("Привет, ");
+   Console.WriteLine(username);
+}
